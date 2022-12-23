876b019cdd416afc873eeeab7692ad46f1787864
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2022-12-23 18:08:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector('.arrow').addEventListener('click', ()=>{
+    document.querySelector('.inner').classList.toggle('open')
+})
+
+document.querySelector('.arrow').addEventListener('click', ()=>{
+    document.querySelector('.arrow').classList.toggle('rotate')
+})
